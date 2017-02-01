bca830b0e167e49fc7803ff559129eca7adc0916
Hoopou <vincentboutot@live.ca>
2017-01-31 19:28:10 -0500

    subsystem grimpeur

--- src/org/usfirst/frc/team5865/robot/Robot.java
+++ src/org/usfirst/frc/team5865/robot/Robot.java
@@ -96,6 +96,11 @@ public class Robot extends IterativeRobot {
 		// this line or comment it out.
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
+		
+		
+		
+		
+		
 	}
 
 	/**
---
+++ src/org/usfirst/frc/team5865/robot/subsystems/Grimpeur.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team5865.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Grimpeur extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
