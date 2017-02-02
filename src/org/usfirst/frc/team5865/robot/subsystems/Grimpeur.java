f84d5a114f7ca598fc158a91d5d826a5efb94d41
Hoopou <vincentboutot@live.ca>
2017-02-02 08:08:02 -0500

    petite modification CAN du Grimpeur

--- src/org/usfirst/frc/team5865/robot/subsystems/Grimpeur.java
+++ src/org/usfirst/frc/team5865/robot/subsystems/Grimpeur.java
@@ -1,18 +1,30 @@
 package org.usfirst.frc.team5865.robot.subsystems;
 
+import org.usfirst.frc.team5865.joystick.XboxControllerUD;
+import org.usfirst.frc5865.RobotMap;
+
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 /**
  *
  */
 public class Grimpeur extends Subsystem {
-
+	
+	private XboxControllerUD piloteXbox;
+	public static CANTalon grimpeurCANTalonDrive;
+	
+	private int stateGrimpeur = 0;
+	
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    	grimpeurCANTalonDrive = new CANTalon(3);    /////// à modifier (le no du CAN)
+         LiveWindow.addActuator("Grimpeur", "CANTalonGrimpeur", grimpeurCANTalonDrive);
     }
 }
 
