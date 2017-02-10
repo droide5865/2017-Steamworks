3eecfa61d2293e34bf66feb7b157ff2ab0e838a0
Hoopou <vincentboutot@live.ca>
2017-02-10 12:42:45 -0500

    modification mineur (constante)

--- src/org/usfirst/frc/team5865/robot/Const.java
+++ src/org/usfirst/frc/team5865/robot/Const.java
@@ -31,12 +31,12 @@ public class Const {
 	public static final double GRIMPEUR_TIME_BEFORE_ACTIVATION = 30;
 	
 	// gobeur
-	public static final double GOBEUR_MAX_SPEED = 0.1;
+	public static final double GOBEUR_MAX_SPEED = 0.5;
 	
 	// lanceur
-	public static final double LANCEUR_DEF_SPEED = 0.8;
-	public static final double LANCEUR_INCREMENT_SPEED = 0.1;
-	public static final double LANCEUR_MAX_SPEED = 1;
+	public static final double LANCEUR_DEF_SPEED = -1;
+	public static final double LANCEUR_INCREMENT_SPEED = -0.1;
+	public static final double LANCEUR_MAX_SPEED = -1;
 	
 	public static final double LANCEUR_DEF_ANGLE = 0.5;
 	public static final double LANCEUR_MIN_ANGLE = 0.2;
